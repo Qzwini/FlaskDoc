4c444e6d15ae751405a4482ec38d9ab6868f7c94
Qzwini <qzwini@gmail.com>
2020-07-13 23:44:10 +0300

    Flask templates, bootstrap, passing parameters to html

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask,render_template,request
 
 #  create flask opject
-app = Flask('MyFirstApp')
+app = Flask('__name__')
 
 # create web page
 @app.route('/')
@@ -10,19 +10,27 @@ app = Flask('MyFirstApp')
 def index():
     contactMail = 'qzwini@gmail.com'
     return render_template('index.html',contact=contactMail)
-
+    
 
 
 @app.route('/sum')
 def sum():
     x=request.args.get('x')
     y=request.args.get('y')
-    return '{} + {} = {}'.format(x,y,int(x)+int(y))
+    sum = int(x)+int(y)
+    return render_template('sum.html',x=x,y=y,sum=sum)
+
+@app.route('/help')
+def fav():
+    return render_template('help.html', lol='FuckU')
+
 
 
 # run App
 if __name__ == "__main__":
     app.run(debug=True)
+
+
 
 '''
 http message code
